4051d34067b661475ccd785a20527eba516ef113
yahya <yahyamokaied@gmail.com>
2020-01-19 19:11:39 +0100

    Fix Buggs

--- ObjCLabb1/ViewController.m
+++ ObjCLabb1/ViewController.m
@@ -12,8 +12,6 @@
 
 @property (strong, nonatomic) IBOutlet UILabel *changeLabel;
 @property (strong, nonatomic) IBOutlet UIButton *appNameButtonHome;
-@property (strong, nonatomic) IBOutlet UIButton *appNameButtonAboutMe;
-
 
 
 
@@ -33,7 +31,7 @@
     [self.changeLabel setText:@"It's Black Now"];
     
     [self.appNameButtonHome setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    [self.appNameButtonAboutMe setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.view  endEditing:YES];
