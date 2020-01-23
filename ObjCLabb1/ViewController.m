3aebd6f533c79535def019ade5dda417dbe14f67
yahya <yahyamokaied@gmail.com>
2020-01-23 20:34:25 +0100

    Game is Working Now

--- ObjCLabb1/ViewController.m
+++ ObjCLabb1/ViewController.m
@@ -12,15 +12,51 @@
 
 @property (strong, nonatomic) IBOutlet UILabel *changeLabel;
 @property (strong, nonatomic) IBOutlet UIButton *appNameButtonHome;
-
-
-
-
+@property (strong, nonatomic) IBOutlet UILabel *Resault;
+@property (weak, nonatomic) IBOutlet UITextField *InputNumber;
+@property (strong, nonatomic) NSString *theNumber;
 
 
 @end
 
 @implementation ViewController
+
+
+- (IBAction)GetNewNumber:(id)sender {
+
+    int rndValue = arc4random_uniform(100)+1;
+    
+    self.theNumber = [NSString stringWithFormat: @"%d", rndValue];
+    NSLog(@"%@", self.theNumber);
+    [self.Resault setText:@"NEW NUMBER ISSUED, GUESS IT NOW"];
+    [self.Resault setTextColor:[UIColor grayColor]];
+    
+
+
+
+}
+
+- (IBAction)GuessTheNumber:(id)sender {
+
+    if ([self.theNumber  isEqualToString:[self.InputNumber text]]) {
+        [self.view  endEditing:YES];
+        [self.Resault setText:[self.InputNumber text]];
+        [self.Resault setTextColor:[UIColor greenColor]];
+    }
+    else
+    {
+        [self.view  endEditing:YES];
+        [self.Resault setText:@"Nooo"];
+        NSLog(self.theNumber);
+        [self.Resault setTextColor:[UIColor redColor]];
+    }
+
+
+}
+
+
+
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -31,7 +67,7 @@
     [self.changeLabel setText:@"It's Black Now"];
     
     [self.appNameButtonHome setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-
+    
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self.view  endEditing:YES];
